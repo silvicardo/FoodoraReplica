8441c36e179f2f6390d8104812f32b7fee695d0e
silvicardo <silvicardo@gmail.com>
2019-01-06 12:18:31 +0100

    responsive navbar

--- main.js
+++ main.js
@@ -11,6 +11,15 @@ console.log('welcome to FoodoraReplica');
 
 $(document).ready(function () {
 
+  //Gestione della navBar
+  $(window).scroll(function () {
+    if ($(window).scrollTop() > 0) {
+      $('nav').addClass('scrolling');
+    } else {
+      $('nav').removeClass('scrolling');
+    }
+  });
+
 });
 
 /**********************************/
