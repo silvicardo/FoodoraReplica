5af1fed1e97847f1c71fca284152b1a9f4313bf8
silvicardo <silvicardo@gmail.com>
2019-01-07 17:06:44 +0100

    hero section

--- main.js
+++ main.js
@@ -14,11 +14,18 @@ $(document).ready(function () {
   //Gestione della navBar
   $(window).scroll(function () {
     if ($(window).scrollTop() > 0) {
-      $('nav').addClass('scrolling');
+      $('nav').addClass('scrolling').addClass('shadow');
     } else {
-      $('nav').removeClass('scrolling');
+      $('nav').removeClass('scrolling').removeClass('shadow');
     }
   });
+
+  //Gestione animazione placeholder
+  $('.hero_section .searchBar input').focus(function () {
+    $('.hero_section .searchBar .inputTitle').addClass('active');
+  }).focusout(function () {
+      $('.hero_section .searchBar .inputTitle').removeClass('active');
+    });
 
 });
 
